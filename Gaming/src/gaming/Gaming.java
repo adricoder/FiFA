3126b50775518ae8f59ca686bcfaf43e4fad99d5
adricoder <adriankimaru@gmail.com>
2018-05-23 12:47:04 +0300

    correct first
    yes

---
+++ Gaming/src/gaming/Gaming.java
@@ -0,0 +1,12 @@
+package gaming;
+
+public class Gaming {
+
+    public Gaming(){
+    
+    }
+    public static void main(String[] args) {
+    
+    }
+    
+}
---
+++ Gaming/src/gaming/Station.java
@@ -0,0 +1,39 @@
+package gaming;
+
+
+public class Station {
+    
+    public int station_id;
+    public String time;
+    public boolean status;
+    
+    public int getStation_id() {
+        return station_id;
+    }
+
+    public void setStation_id(int station_id) {
+        this.station_id = station_id;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+        
+    public Station(){
+    
+    }
+    
+}
