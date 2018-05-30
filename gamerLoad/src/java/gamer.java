db21c32468dd142640fccded492aa81827f5928c
adricoder <adriankimaru@gmail.com>
2018-05-30 13:03:48 +0300

    Web_intro
    first step

---
+++ gamerLoad/src/java/gamer.java
@@ -0,0 +1,27 @@
+public class gamer {
+    
+    public int gamer_id;
+    public String name;
+    
+    
+    public gamer(){
+    
+    }
+
+    public int getGamer_id() {
+        return gamer_id;
+    }
+
+    public void setGamer_id(int gamer_id) {
+        this.gamer_id = gamer_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
---
+++ gamerLoad/src/java/session.java
@@ -0,0 +1,26 @@
+public class session {
+     
+    public int session_id;
+    public String duration;
+    
+    public session(){
+    
+    }
+
+    public int getSession_id() {
+        return session_id;
+    }
+
+    public void setSession_id(int session_id) {
+        this.session_id = session_id;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+    
+}
